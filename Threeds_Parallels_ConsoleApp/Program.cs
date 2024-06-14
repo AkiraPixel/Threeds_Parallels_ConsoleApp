51036d24e3dca5bd15a9a8b67475ce8354344206
linkomizin <linkomizin@gmail.com>
2024-06-14 22:00:10 +0300

    initial

---
+++ Threeds_Parallels_ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Threeds_Parallels_ConsoleApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Method Main Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Please press any key...");
+            Console.ReadKey();
+
+
+
+            ThreadPoolWorker threadPoolWorker = new ThreadPoolWorker(PrintChar);
+
+            threadPoolWorker.Start('*');
+
+            threadPoolWorker.Wait();
+
+            // получение результата
+            if (threadPoolWorker.Success is true)
+            {
+                Console.WriteLine();
+                Console.WriteLine((int)threadPoolWorker.Result);
+            }
+
+
+        }
+
+        #region MyRegion
+ 
+        /*
+                private static void Example1(object state)
+                {
+                    Console.WriteLine($"Method Example1 begean work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Method Example1 end work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+                }
+
+                private static void Example2(object state)
+                {
+                    Console.WriteLine($"Method Example2 begean work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Method Example2 end work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+                }
+
+                private static void Report()
+                {
+                    ThreadPool.GetMaxThreads(out int maxWorkThreads, out int maxPortThreads);
+                    ThreadPool.GetAvailableThreads(out int workThreads, out int portThreads);
+
+                    Console.WriteLine($"work thread {workThreads} from {maxWorkThreads}");
+                    Console.WriteLine(new string('-', 80));
+                }*/
+
+        #endregion
+        private static int PrintChar(object arg)
+        {
+            Console.WriteLine($"Current thread Id: ${Thread.CurrentThread.ManagedThreadId}");
+            char item = (char)arg;
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(item);
+                Thread.Sleep(1000);
+            }
+
+            return 5;
+        }
+    }
+}
+
---
+++ Threeds_Parallels_ConsoleApp/ThreadPoolWorker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threeds_Parallels_ConsoleApp
+{
+    public class ThreadPoolWorker
+    {
+        private readonly Func<object, int> _action;
+
+        public ThreadPoolWorker(Func<object, int> action)
+        {
+            _action = action ?? throw new ArgumentNullException();
+        }
+
+        public bool Success { get; private set; } = false;
+        public bool IsComplete { get; private set; } = false;
+        public object Result { get; private set; }
+
+
+        public void Start(object state)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(Execute), state);
+        }
+
+        public void Wait()
+        {
+            Console.WriteLine($"Wait start in {Thread.CurrentThread.ManagedThreadId}");
+            while (!IsComplete)
+            {
+                Thread.Sleep(150);
+            }
+        }
+
+        private void Execute(object state)
+        {
+            try
+            {
+                Console.WriteLine($"Execute start in {Thread.CurrentThread.ManagedThreadId}");
+                Result = _action.Invoke(state);
+                Success = true;
+            }
+            catch (Exception ex)
+            {
+                Success = false;
+            }
+            finally
+            {
+                IsComplete = true;
+
+            }
+        }
+    }
+}
